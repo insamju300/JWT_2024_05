3908a7b592e710dcbdc65fcc8473886599ae52f9
insamju300 <insamju300@gmail.com>
2024-05-14 09:18:42 +0900

    first commit

---
+++ src/main/java/com/korea_it/jwt_2024_05/Jwt202405Application.java
@@ -0,0 +1,13 @@
+package com.korea_it.jwt_2024_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt202405Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwt202405Application.class, args);
+    }
+
+}
